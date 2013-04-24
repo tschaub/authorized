baa2e5a587301d22c047b57d574179b5e45696a9
Tim Schaub <tim.schaub@gmail.com>
2013-04-24 17:54:04 -0600

    Discourage tampering with cached view properties (closes #4)
    This is not meant to make the view completely tamper-proof, only to avoid accidental modification.

--- test/lib/view.spec.js
+++ test/lib/view.spec.js
@@ -14,6 +14,15 @@ describe('View', function() {
       var view = new View();
       assert.instanceOf(view, View);
     });
+
+    it('creates a frozen object', function() {
+      var view = new View();
+      view.foo = 'bar';
+      assert.isUndefined(view.foo);
+      var before = view.actions;
+      view.actions = {};
+      assert.strictEqual(view.actions, before);
+    });
   });
 
   describe('#actions', function() {
@@ -28,13 +37,13 @@ describe('View', function() {
       var view = new View();
       view.actions['edit page'] = true;
       view.actions['delete page'] = false;
-      assert.strictEqual(view.can('edit page'), true);
-      assert.strictEqual(view.can('delete page'), false);
+      assert.isTrue(view.can('edit page'));
+      assert.isFalse(view.can('delete page'));
     });
 
     it('returns undefined if unknown action', function() {
       var view = new View();
-      assert.strictEqual(view.can('do nothing'), undefined);
+      assert.isUndefined(view.can('do nothing'));
     });
   });
 
@@ -42,6 +51,50 @@ describe('View', function() {
     it('is an object', function() {
       var view = new View();
       assert.isObject(view.entities);
+    });
+  });
+
+  describe('#freeze()', function() {
+    it('prevents cached actions from being modified', function() {
+      var view = new View();
+      view.actions['do anything'] = true;
+      assert.isTrue(view.can('do anything'));
+
+      view.freeze();
+      view.actions['do anything'] = false;
+      assert.isTrue(view.can('do anything'));
+
+      view.actions['view passwords'] = true;
+      assert.isUndefined(view.can('view passwords'));
+    });
+
+    it('prevents cached entities from being modified', function() {
+      var view = new View();
+      var page = {};
+      view.entities.page = page;
+      assert.strictEqual(view.get('page'), page);
+
+      view.freeze();
+      view.entities.page = {};
+      assert.strictEqual(view.get('page'), page);
+
+      view.entities.organization = {};
+      assert.isNull(view.get('organization'));
+    });
+
+    it('prevents cached roles from being modified', function() {
+      var view = new View();
+      view.roles['admin'] = false;
+      view.roles['page.author'] = true;
+      assert.isFalse(view.has('admin'));
+      assert.isTrue(view.has('page.author'));
+
+      view.freeze();
+      view.roles['admin'] = true;
+      assert.isFalse(view.has('admin'));
+
+      view.roles['site.owner'] = true;
+      assert.isUndefined(view.has('site.owner'));
     });
   });
 
@@ -55,7 +108,7 @@ describe('View', function() {
 
     it('returns null if none found', function() {
       var view = new View();
-      assert.strictEqual(view.get('foo'), null);
+      assert.isNull(view.get('foo'));
     });
   });
 
@@ -64,13 +117,13 @@ describe('View', function() {
       var view = new View();
       view.roles.admin = false;
       view.roles['page.author'] = true;
-      assert.strictEqual(view.has('admin'), false);
-      assert.strictEqual(view.has('page.author'), true);
+      assert.isFalse(view.has('admin'));
+      assert.isTrue(view.has('page.author'));
     });
 
     it('returns undefined if unknown role', function() {
       var view = new View();
-      assert.strictEqual(view.has('unknown'), undefined);
+      assert.isUndefined(view.has('unknown'));
     });
 
     it('accepts a Role instance', function() {
@@ -79,8 +132,8 @@ describe('View', function() {
       var author = new Role('page.author');
       view.roles[admin.name] = false;
       view.roles[author.name] = true;
-      assert.strictEqual(view.has(admin), false);
-      assert.strictEqual(view.has(author), true);
+      assert.isFalse(view.has(admin));
+      assert.isTrue(view.has(author));
     });
   });
 
